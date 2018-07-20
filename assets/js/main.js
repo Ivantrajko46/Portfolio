d167c0c70591cf1b805f246f695e97a9a7977825
Ivan Trajkovic <32568927+Ivantrajko46@users.noreply.github.com>
2018-07-20 11:24:50 +0200

    img optimization and favicon

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,29 +1,65 @@
 $(function() {
+
     $(window).scroll(function() {
         var scroll = $(window).scrollTop();
         if (scroll > 300) {
-            $(".navbar").css("background", "rgba(0,0,0,.8)");
+            $(".navbar").css("background", "rgba(0,0,0,.85)");
             $(".navbar").css("boxShadow", "-4px 5px 15px #000");
 
-       
+
         } else {
             $(".navbar").css("background", "transparent");
             $(".navbar").css("boxShadow", "");
         }
     });
 
+    $("a.nav-item").click(function() {
+        $("button.myToggler").trigger("click");
+    });
+    //SCROLL ANIMATION FOR NALBAR LINKS
 
-    
-    $('#skills').mouseover(function(e){
-    	$('div.progress-bar[aria-valuenow="80"]').css("width", "80%");
-    	$('div.progress-bar[aria-valuenow="70"]').css("width", "70%");
+    $(".navbar-nav a[href^='#").click(function(e) {
+        e.preventDefault(); //
+
+        var target = this.hash;
+        var $target = $(target);
+
+        $("html, body").animate({
+            scrollTop: $target.offset().top
+        }, 1000, );
+
     });
-    $('#skills').mouseout(function(e){
-    	$('div.progress-bar[aria-valuenow="80"]').css("width", "");
-    	$('div.progress-bar[aria-valuenow="70"]').css("width", "");
+    // BACK TO TOP ARROW
+    $('#top i.fas').click(function() {
+        $("html, body").animate({ scrollTop: 0 }, 1000);
     });
 
-    $("a.nav-item").click(function(){
-    	$("button.myToggler").trigger("click");
+    // PROGRESS BAR HOVER
+    $('#skills').mouseover(function(e) {
+        $('div.progress-bar[aria-valuenow="80"]').css("width", "80%");
+        $('div.progress-bar[aria-valuenow="70"]').css("width", "70%");
     });
+    $('#skills').mouseout(function(e) {
+        $('div.progress-bar[aria-valuenow="80"]').css("width", "");
+        $('div.progress-bar[aria-valuenow="70"]').css("width", "");
+    });
+
+    // PORTFOLIO PROJECTS HOVER
+    $("div .projects").each(function(index, item) {
+        $(this).on('mouseover', function(e) {
+            $(item).addClass("animated");
+            $(item).addClass("jello");
+
+        });
+        $(this).on('mouseout', function(e) {
+            $(item).removeClass("animated");
+            $(item).removeClass("jello");
+
+
+        });
+    });
+
+
+
+
 });
