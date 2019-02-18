5a8dd57e1dcd9e584a87f94e0fb1349c7cd9733e
Ivan Trajkovic <trajkovicivan1@gmail.com>
2019-02-18 17:25:10 +0100

    Updated data

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,41 +1,38 @@
 $(function() {
 
+
     $(window).scroll(function() {
         var scroll = $(window).scrollTop();
         if (scroll > 250) {
             $(".navbar").css("background", "rgba(0,0,0,.85)");
             $(".navbar").css("boxShadow", "-4px 5px 15px #000");
-
-
         } else {
             $(".navbar").css("background", "transparent");
             $(".navbar").css("boxShadow", "");
         }
     });
 
-
     $("a.nav-item").click(function() {
         $("button.myToggler").trigger("click");
     });
     //SCROLL ANIMATION FOR NAVBAR LINKS
-
     $(".navbar-nav a[href^='#").click(function(e) {
         e.preventDefault(); //
 
         var target = this.hash;
         var $target = $(target);
 
+        console.log(target);
+        console.log($target);
+
         $("html, body").animate({
             scrollTop: $target.offset().top
         }, 1000, );
-
     });
     // BACK TO TOP ARROW
     $('#top i.fas').click(function() {
         $("html, body").animate({ scrollTop: 0 }, 1000);
     });
-
-
     // PORTFOLIO PROJECTS HOVER
     $("div .projects").each(function(index, item) {   
         $(this).on('mouseover', function(e) {
@@ -44,15 +41,14 @@ $(function() {
             $(item).addClass("jello");
             // $(aboutProject).slideDown();
             // console.log($(this));
-
         });
         $(this).on('mouseout', function(e) {
             // let aboutProject = $(e.target.lastElementChild);
             $(item).removeClass("animated");
             $(item).removeClass("jello");
             // $(aboutProject).slideUp();
-
-
         });
     });
+
+
 });
